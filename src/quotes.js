855b0db710ea42f14a2eabf97149475aadc6ced5
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-27 23:34:05 -0500

    Base de datos de frases celebres

---
+++ src/quotes.js
@@ -0,0 +1,44 @@
+const quotes = [
+  {
+    quote: "No hay caminos para la paz; la paz es el camino.",
+    author: "Mahatma Gandhi"
+  },
+  {
+    quote: "Hay dos cosas que son infinitas: el universo y la estupidez humana; de la primera no estoy muy seguro.",
+    author: "Albert Einstein"
+  },
+  {
+    quote: "No abras los labios si no estás seguro de que lo que vas a decir es más hermoso que el silencio.",
+    author: "Proverbio árabe"
+  },
+  {
+    quote: "El pesimista se queja del viento; el optimista espera que cambie; el realista ajusta las velas.",
+    author: "William George Ward"
+  },
+  {
+    quote: "Podrán cortar todas las flores, pero no podrán detener la primavera.",
+    author: "Pablo Neruda"
+  },
+  {
+    quote: "Algún día en cualquier parte, en cualquier lugar indefectiblemente te encontrarás a ti mismo, y ésa, sólo ésa, puede ser la más feliz o la más amarga de tus horas.",
+    author: "Pablo Neruda"
+  },
+  {
+    quote: "Tu mejor activo es el tiempo. Cada segundo perdido, es vida perdida, y nuestro paso por el mundo es tan breve, que no me hago a la idea de no aprovechar cada momento.",
+    author: "Facundo Cabral"
+  },
+  {
+    quote: "Haz sólo lo que amas y serás feliz, y el que hace lo que ama, está benditamente condenado al éxito, que llegará cuando deba llegar, porque lo que debe ser será, y llegará naturalmente.",
+    author: "Facundo Cabral"
+  },
+  {
+    quote: " El único hombre que no se equivoca es el que nunca hace nada",
+    author: "Goethe"
+  },
+  {
+    quote: "Reunirse es un comienzo, permanecer juntos es el progreso y trabajar juntos es el éxito.",
+    author: "Henry Ford"
+  }
+];
+
+export default quotes;
