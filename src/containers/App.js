937fbcdd2d2ac755f4b6b3110cd4d6facedb5583
Malena Ramirez <maramirezuribe@gmail.com>
2021-06-27 23:33:17 -0500

    Esquema inicial de los elementos de la página

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin:0;
+    font-family: 'Akaya Kanadaka', cursive;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-      <h1>Hola</h1>
+      <GlobalStyle />
+      <div id="text-Container">
+        <div>Aqui va un icono</div>
+        <div id="text">Aqui va el texto</div>
+      </div>
+      <p id="author">Aquí va el autor</p>
+      <button id="new-quote" type="button">
+        Nueva frase
+      </button>
+      <div>
+        <a href="http:/twitter.com/intent/tweet" target="_blank" rel="noopener noreferrer" id="tweet-quote">
+          Icono de twitter
+        </a>
+      </div>
     </div>
   );
 }
